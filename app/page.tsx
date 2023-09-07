27c7fabdea3c262783e8c670020926ab9f13b79e
WooFoo <sujanstha3220@gmail.com>
2023-09-07 16:22:32 +0545

    Added authorization with graphql

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
 import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
 import { BiHomeCircle, BiHash, BiUser, BiMoney } from "react-icons/bi";
 import { SlOptions } from "react-icons/sl";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import FeedCard from "@/components/FeedCard";
+import { useCallback } from "react";
+import { toast } from "react-hot-toast";
+import { graphqlClient } from "@/clients/api";
+import { verifyUserGoogleTokenQuery } from "@/graphql/queries/user";
 
 interface TwitterSidebarButton {
 	title: string;
@@ -44,6 +50,26 @@ const sidebarMenuItems: TwitterSidebarButton[] = [
 ];
 
 export default function Home() {
+	const handleLoginWithGoole = useCallback(
+		async (cred: CredentialResponse) => {
+			const googleToken = cred.credential;
+			if (!googleToken) return toast.error(`Google token not found`);
+
+			const { verifyGoogleToken } = await graphqlClient.request(
+				verifyUserGoogleTokenQuery,
+				{
+					token: googleToken,
+				}
+			);
+
+			toast.success("Verified Success");
+			console.log(verifyGoogleToken);
+			if (verifyGoogleToken)
+				window.localStorage.setItem("twitter_token", verifyGoogleToken);
+		},
+		[]
+	);
+
 	return (
 		<div>
 			<div className="grid grid-cols-12 gap-3 h-screen w-screen px-32">
@@ -86,7 +112,13 @@ export default function Home() {
 					<FeedCard />
 					<FeedCard />
 				</div>
-				<div className="col-span-3 "></div>
+				{/* google login button  */}
+				<div className="col-span-3 p-5">
+					<div className=" p-5 bg-slate-700 rounded-lg">
+						<h1 className="my-2 text-2xl">New to Twitter</h1>
+						<GoogleLogin onSuccess={handleLoginWithGoole} />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
---
+++ graphql/queries/user.ts
@@ -0,0 +1,9 @@
+import { graphql } from "../../gql";
+export const verifyUserGoogleTokenQuery = graphql(
+	`
+		#graphql
+		query VerifyUserGoogleToken($token: String!) {
+			verifyGoogleToken(token: $token)
+		}
+	`
+);
